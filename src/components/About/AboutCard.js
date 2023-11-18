7c2f87480cda8217390fb1fa329c4e41a7357a4f
Dr-dyrane <halodyrane@gmail.com>
2023-11-18 20:38:31 +0100

    new file:   src/Assets/Projects/dddc.png

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -3,42 +3,43 @@ import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
-  return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Alexander Udeogaranya</span>
-            from <span className="purple"> Lagos, Nigeria.</span>
-            <br />
-            I am currently employed as a Medical Doctor at NNRH OJO.
-            <br />
-            I have completed Software Engineering in ALX SE Program and MBBS at Madonna University
-            .
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Designing
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+	return (
+		<Card className="quote-card-view">
+			<Card.Body>
+				<blockquote className="blockquote mb-0">
+					<p style={{ textAlign: "justify" }}>
+						Hi Everyone, I am{" "}
+						<span className="purple">Alexander Udeogaranya </span>
+						from <span className="purple">Lagos, Nigeria.</span>
+						<br />
+						I am currently employed as a Medical Doctor at NNRH OJO.
+						<br />
+						I have completed Software Engineering in ALX SE Program and MBBS at
+						Madonna University.
+						<br />
+						<br />
+						Apart from coding, some other activities that I love to do!
+					</p>
+					<ul>
+						<li className="about-activity">
+							<ImPointRight /> Playing Games
+						</li>
+						<li className="about-activity">
+							<ImPointRight /> Designing
+						</li>
+						<li className="about-activity">
+							<ImPointRight /> Travelling
+						</li>
+					</ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Dyrane</footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
-  );
+					<p style={{ color: "rgb(155 126 172)" }}>
+          "The concept of 'impossible' is relevant only in acknowledging that, in the realm of possibilities, nothing is truly impossible!"
+					</p>
+					<footer className="blockquote-footer">Dyrane</footer>
+				</blockquote>
+			</Card.Body>
+		</Card>
+	);
 }
 
 export default AboutCard;
