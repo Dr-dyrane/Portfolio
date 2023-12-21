30404d936af1583e35dc3b97cdfeb22f68fa02eb
Dr-dyrane <halodyrane@gmail.com>
2023-12-21 08:54:13 +0100

    new file:   src/components/Projects/builds.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,35 +6,34 @@ import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
+    <Card className="project-card-view" style={{ borderRadius: 20 }}>
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ height: "350px", objectFit: "cover", borderRadius: "20px 20px 0 0" }} />
+      <Card.Body style={{ height: "250px", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "" }}>
+        <Card.Text style={{ fontSize: "0.8rem", overflow: "hidden", textOverflow: "ellipsis" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+        <div >
+          <Button variant="primary" href={props.ghLink} target="_blank" style={{ borderRadius: 10, marginBottom: "5px", marginRight: 5 }}>
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
           </Button>
-        )}
+
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ borderRadius: 10, marginBottom: "5px", marginLeft:5 }}
+            >
+              <CgWebsite /> &nbsp;
+              {"Demo"}
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,19 +1,15 @@
+// Projects.js
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import dddc from "../../Assets/Projects/med.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import dfc from "../../Assets/Projects/dfc.png";
+import builds from "./builds";
 
 function Projects() {
   return (
     <Container fluid className="project-section">
       <Particle />
-      <Container>
+      <Container className="project-container">
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
@@ -21,72 +17,18 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={dddc}
-              isBlog={false}
-              title="Dose Calculator"
-              description="Dose Calculator is a simple React application that calculates prescription doses based on weight or age. The application allows users to input either the weight or age of a child and generates a prescription summary."
-              ghLink="https://github.com/Dr-dyrane/drug"
-              demoLink="https://med.dyrane.live"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={dfc}
-              isBlog={false}
-              title="Dyrane's Farm Cast"
-              description="Dyrane's Farm Cast is a web application designed to provide essential weather information and tailored agricultural recommendations to empower farmers and agriculture enthusiasts in making informed decisions. This MVP specification outlines the key components of the project."
-              ghLink="https://github.com/Dr-dyrane/dfc"
-              demoLink="https://dfc.dyrane.live"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+          {builds.map((project, index) => (
+            <Col md={4} className="project-card" key={index}>
+              <ProjectCard
+                imgPath={project.imgPath}
+                isBlog={project.isBlog}
+                title={project.title}
+                description={project.description}
+                ghLink={project.ghLink}
+                demoLink={project.demoLink}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </Container>
